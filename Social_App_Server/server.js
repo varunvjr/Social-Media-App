6cef227137dccd1ec7099a6e54c3a6a4ce185908
varunvjr <varun.vjr@gmail.com>
2021-07-02 17:49:23 +0530

    error.js has been added to controller

--- Social_App_Server/server.js
+++ Social_App_Server/server.js
@@ -2,21 +2,22 @@ import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from 'body-parser';
+import {errorHandler} from "./controller/error.js"
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT;
 app.use(cors());
-app.use(bodyParser.json());
+
 app.get("/",(req,res)=>{
     res.send("Social App Server")
 })
-
+app.use(bodyParser.json());
 app.use((req,res,next)=>{
     let err=new Error("Not found");
-    res.json({
-        message:err.message
-    })
+    err.status=404;
+   next(err);
 })
+app.use(errorHandler);
 app.listen(PORT,()=>{
     console.log(`Server running on port:${PORT}`);
 })
