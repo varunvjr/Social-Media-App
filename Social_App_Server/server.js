8bb6284b655cbf1b73b6173322dfe975ebd0725f
varunvjr <varun.vjr@gmail.com>
2021-07-02 17:09:58 +0530

    Error handler added

--- Social_App_Server/server.js
+++ Social_App_Server/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from 'body-parser';
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT;
+app.use(cors());
+app.use(bodyParser.json());
 app.get("/",(req,res)=>{
     res.send("Social App Server")
 })
 
+app.use((req,res,next)=>{
+    let err=new Error("Not found");
+    res.json({
+        message:err.message
+    })
+})
 app.listen(PORT,()=>{
     console.log(`Server running on port:${PORT}`);
 })
